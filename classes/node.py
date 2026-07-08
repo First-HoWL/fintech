a64ef2a9fc77281de4b360ffb0f699daa9789830
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2026-07-08 20:22:33 +0300

    added node, added flask with config.json
    Co-authored-by: Dineanad <MishaPetrovskui@users.noreply.github.com>

---
+++ classes/node.py
@@ -0,0 +1,56 @@
+from classes.blockchain import Blockchain
+from classes.block import Block
+from classes.miner import Miner
+import requests as request
+
+class Node:
+    def __init__(self, name, apis, difficulty):
+        self.name = name
+        self.blockchain = Blockchain(difficulty=difficulty)
+        self.apis = apis
+    
+    def get_chain(self):
+        return self.blockchain
+    
+    def post_chain(self, chain):
+        if self.blockchain.chain.__len__() >= chain.chain.__len__():
+            pass
+        elif not self.blockchain.chain.is_chain_valid():
+            print(f'{self.name} received an invalid chain')
+            if chain.is_chain_valid():
+                self.blockchain = chain
+        else:
+            self.blockchain = chain
+            print(f'{self.name} received a valid chain (length: {chain.chain.__len__()} blocks)')
+    
+    def get_apis(self):
+        return self.apis
+    
+    def post_apis(self, apis):
+        for api in apis:
+            if not self.apis.__contains__(api):
+                self.apis.append(api)
+
+    def request_chain(self, api):
+        response = request.get(f'{api}/api/chain')
+        if response.status_code != 200:
+            print(f'Error requesting chain from {api}: {response.status_code}')
+            return
+        
+        data = response.json()
+        received_blockchein = Blockchain()
+        received_blockchein.difficulty = self.blockchain.difficulty
+        received_blockchein.chain = [Block.from_dict(d) for d in data['chain']]
+        self.post_chain(received_blockchein)
+    
+    def request_apis(self, api):
+        response = request.get(f'{api}/api/apis')
+        if response.status_code != 200:
+            print(f'Error requesting apis from {api}: {response.status_code}')
+            return
+        
+        data = response.json()
+        self.post_apis(data["apis"])
+
+    def post_block(self, block_data):
+        return self.blockchain.add_block(block_data)    
--- main.py
+++ main.py
@@ -1,26 +1,73 @@
 from classes.blockchain import Blockchain
 from classes.block import Block
 from classes.miner import Miner
+from classes.node import Node
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 import time
+import threading
+import json
 
-def main():
-    difficulty = 5
-    blockchain = Blockchain(difficulty)
+app = Flask(__name__)
+CORS(app)
+
+config = {}
+
+with open("data/config.json", "r", encoding="utf-8") as file:
+    config = json.load(file)
+
+difficulty = 4
+node = Node(name=config.get("name", "node_1"), apis=config.get("apis", []), difficulty=difficulty)
+
+port=config.get("port")
+delay = config.get("delay") # ms
+
+def task():
     miner = Miner(name='miner_001')
-    
-
+        
     for i in range(1, 4):
         new_block = Block(
             index=i,
             timestamp=time.time(),
             data=f'Дані блоку #{i}',
-            previous_hash=blockchain.last_block.hash,
+            previous_hash=node.blockchain.last_block.hash,
         )
         mined_block = miner.mine_block(new_block, difficulty)
-        blockchain.add_block(mined_block)
+        node.post_block(mined_block)
+    while True:
+        for api in node.apis:
+            try:
+                node.request_chain(api)
+            except Exception as e:
+                print(f"Помилка при запиті до API {api}: {e}")
+            
+            try:
+                node.request_apis(api)
+            except Exception as e:
+                print(f"Помилка при запиті до API {api}: {e}")
+                
+        time.sleep(delay)
 
-    blockchain.print_chain()
-    print('Ланцюг валідний:', blockchain.is_chain_valid())
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+@app.route("/api/get_apis")
+def get_apis():
+    return {
+        "success": True,
+        "apis": node.get_apis()
+        }
+
+@app.route("/api/chain", methods=["GET"])
+def get_chain():
+    dict_of_chain = [d.to_dict() for d in node.blockchain.chain]
+    return jsonify({'chain': dict_of_chain})
+
+def main():
+    app.run(debug=True, host="0.0.0.0", port=port)
 
 if __name__ == '__main__':
+    flask_thread = threading.Thread(target=task, daemon=True)
+    flask_thread.start()
     main()
